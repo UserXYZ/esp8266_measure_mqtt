c1f7c100ab8553938bbb8e86349c9fe6939cd369
Andrea Prunic <andrea@yu.net>
2015-08-31 18:12:26 +0200

    try to fix startup

--- init.lua
+++ init.lua
@@ -1,61 +1,5 @@
--- prepare files
-local compileAndRemoveIfNeeded = function(f)
-   if file.open(f) then
-      file.close()
-      print('Compiling:', f)
-      node.compile(f)
-      file.remove(f)
-      collectgarbage()
-   end
-end
-
-local serverFiles = {'message2.lua', 'myds3.lua', 'httpserver.lua', 'httpserver-basicauth.lua', 'httpserver-conf.lua', 'base64dec.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
-for i, f in ipairs(serverFiles) do compileAndRemoveIfNeeded(f) end
-
-compileAndRemoveIfNeeded = nil
-serverFiles = nil
-collectgarbage()
-
 -- start configuration
 local conf = dofile("httpserver-conf.lc")
-
-local wifiConfig = {}
-wifiConfig.mode = wifi.STATION
-wifiConfig.stationPointConfig = {}
-wifiConfig.stationPointConfig.ssid = conf.wlan.ssid
-wifiConfig.stationPointConfig.pwd =  conf.wlan.pwd
-
-wifi.setmode(wifiConfig.mode)
-print('set (mode='..wifi.getmode()..')')
-print('MAC: ',wifi.sta.getmac())
-print('chip: ',node.chipid())
-print('heap: ',node.heap())
-
-wifi.sta.config(wifiConfig.stationPointConfig.ssid, wifiConfig.stationPointConfig.pwd)
-wifiConfig = nil
-collectgarbage()
-
-local joinCounter = 0
-local joinMaxAttempts = 20
-tmr.alarm(1, 5000, 1, function()
-   local ip = wifi.sta.getip()
-   if ip == nil and joinCounter < joinMaxAttempts then
-      print('Connecting to WiFi Access Point ...')
-      joinCounter = joinCounter +1
-   else
-      if joinCounter == joinMaxAttempts then
-         print('Failed to connect to WiFi Access Point.')
-      else
-         print('IP: ',ip)
-         -- Uncomment to automatically start the server in port 80
-         --dofile("httpserver.lc")(8008)
-      end
-      tmr.stop(1)
-      joinCounter = nil
-      joinMaxAttempts = nil
-      collectgarbage()
-   end
-end)
 
 -- start timer measuring and putting results into global table gtab
 local function startTemp()
@@ -102,29 +46,93 @@ local function startMQTT()
         end
     end)
 end
--- main()
 -- start or not
-local abort = false
-print("Do you want to cancel startup in 5 seconds? Y/N [N]")
-uart.on("data", 1, function(data)
-    if string.upper(data)=='Y' then
-        uart.on("data")
-        print("Cancelling startup...")
-        abort = true
-    elseif string.upper(data)=='N' then
-        uart.on("data")
-        print("OK, starting")
-        abort = false
-    else
-        print("Wrong input, starting anyway")
-        uart.on("data")
-        abort = false
-    end
-end,0)
+local function startup()
+    abort = false
+    print("Do you want to cancel startup in 5 seconds? Y/N [N]")
+    uart.on("data", 1, function(data)
+        if string.upper(data)=='Y' then
+            uart.on("data")
+            print("Cancelling startup...")
+            abort = true
+        elseif string.upper(data)=='N' then
+            uart.on("data")
+            print("OK, starting")
+            abort = false
+        else
+            print("Wrong input, starting anyway")
+            uart.on("data")
+            abort = false
+        end
+    end, 0)
+end
+-- prepare files
+local compileAndRemoveIfNeeded = function(f)
+   if file.open(f) then
+      file.close()
+      print('Compiling:', f)
+      node.compile(f)
+      file.remove(f)
+      collectgarbage()
+   end
+end
+-- main()
+local serverFiles = {'message2.lua', 'myds3.lua', 'httpserver.lua', 'httpserver-basicauth.lua', 'httpserver-conf.lua', 'base64dec.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
+for i, f in ipairs(serverFiles) do
+    compileAndRemoveIfNeeded(f)
+end
 
+compileAndRemoveIfNeeded = nil
+serverFiles = nil
+collectgarbage()
+
+local wifiConfig = {}
+wifiConfig.mode = wifi.STATION
+wifiConfig.stationPointConfig = {}
+wifiConfig.stationPointConfig.ssid = conf.wlan.ssid
+wifiConfig.stationPointConfig.pwd =  conf.wlan.pwd
+
+wifi.setmode(wifiConfig.mode)
+print('set (mode='..wifi.getmode()..')')
+print('MAC: ',wifi.sta.getmac())
+print('chip: ',node.chipid())
+print('heap: ',node.heap())
+
+wifi.sta.config(wifiConfig.stationPointConfig.ssid, wifiConfig.stationPointConfig.pwd)
+wifiConfig = nil
+collectgarbage()
+
+local joinCounter = 0
+local joinMaxAttempts = 20
+tmr.alarm(1, 5000, 1, function()
+   local ip = wifi.sta.getip()
+   if ip == nil and joinCounter < joinMaxAttempts then
+      print('Connecting to WiFi Access Point ...')
+      joinCounter = joinCounter +1
+   else
+      if joinCounter == joinMaxAttempts then
+         print('Failed to connect to WiFi Access Point.')
+      else
+         print('IP: ',ip)
+         -- Uncomment to automatically start the server in port 80
+         --dofile("httpserver.lc")(8008)
+startTemp()
+tmr.delay(2000)
+startMQTT()
+print("Started...")
+      end
+      tmr.stop(1)
+      joinCounter = nil
+      joinMaxAttempts = nil
+      collectgarbage()
+   end
+end)
+
+--[[
 tmr.wdclr()
-tmr.alarm(3,1000,1,function()
+tmr.alarm(3,5000,0,function()
     if wifi.sta.status() == 5 then
+        startup()
         if abort == false then
             startTemp()
             tmr.delay(2000)
@@ -134,4 +142,5 @@ tmr.alarm(3,1000,1,function()
         end
     end
 end)
+]]--
 collectgarbage()
