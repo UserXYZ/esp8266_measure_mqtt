28be16a7302a4d7554362a3c0c955363d8b495e0
Andrea Prunic <andrea@yu.net>
2015-10-09 18:07:49 +0200

    - fix callback

--- myNetTime.lua
+++ myNetTime.lua
@@ -1,5 +1,3 @@
--- retrieve the current time from Google
--- tested on NodeMCU 0.9.5 build 20150108
 --[[
 local moduleName = ... 
 local M = {}
@@ -9,8 +7,10 @@ local M
 
 local function getTime(tz,host,cb)
     local time = {}
-    local conn=net.createConnection(net.TCP, 0) 
-    if tz < -12 or tz > 12 or not tz then return nil end
+    local ipaddr
+    local conn=net.createConnection(net.TCP, 0)
+        
+    if tz < -12 or tz > 12 or not tz then cb(nil) end
     
     conn:on("connection",function(conn, payload)
             conn:send("HEAD / HTTP/1.1\r\n".. 
@@ -22,25 +22,34 @@ local function getTime(tz,host,cb)
     
     conn:on("receive", function(conn, payload)
         local gt=string.sub(payload,string.find(payload,"Date: ")+23,
-          string.find(payload,"Date: ")+31)
-        local h=string.sub(gt,1,2)
+            string.find(payload,"Date: ")+31)
+        local h=string.sub(gt,1,2)+tz
         local m=string.sub(gt,4,5)
         local s=string.sub(gt,7,8)
-        h=h+tz
-        if (h+tz) > 23 then h=24-h+tz end
-        if (h+tz)< 0 then h=24+h+tz end
+        if h > 23 then h=24-h+tz end
+        if h < 0 then h=24+h+tz end
 --        time=h..":"..m..":"..s
         time={h,m,s}
-        collectgarbage("collect")
         conn:close()
+        conn=nil
+        collectgarbage("collect")
         cb(time)
     end)
-    
-    conn:connect(80,host)
+
+    conn:dns(tostring(host),function(conn,ip)
+        if ip then
+            ipaddr=tostring(ip)
+            conn:connect(80,ipaddr)
+        else
+            conn:close()
+            conn=nil
+            collectgarbage("collect")
+            print("DNS can't resolve "..host)
+            cb(nil)
+        end
+    end)
 end
 
-M = {
-        getTime = getTime
-    }
+M = { getTime = getTime }
 
 return M
