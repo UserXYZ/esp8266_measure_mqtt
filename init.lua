c52c9aab16f091ae3b0c658d7dc722246ebb9b46
Andrea Prunic <andrea@yu.net>
2015-10-30 16:49:47 +0100

    - don't compile and delete config.lua to enable easier reconfiguration

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ local compileAndRemoveIfNeeded = function(f)
    end
 end
 -- main()
-local serverFiles = {'message2.lua', 'myds3.lua', 'base64dec.lua', 'bmp085.lua', 'myNetTime.lua', 'httpserver.lua', 'httpserver-basicauth.lua', 'config.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
+local serverFiles = {'message2.lua', 'myds3.lua', 'base64dec.lua', 'bmp085.lua', 'myNetTime.lua', 'httpserver.lua', 'httpserver-basicauth.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
 for i, f in ipairs(serverFiles) do
     compileAndRemoveIfNeeded(f)
 end
@@ -28,7 +28,6 @@ wifiConfig.stationPointConfig.pwd =  conf.wlan.pwd
 wifi.setmode(wifiConfig.mode)
 print('set (mode='..wifi.getmode()..')')
 print('MAC: ',wifi.sta.getmac())
-print('chip: ',node.chipid())
 print('heap: ',node.heap())
 
 wifi.sta.config(wifiConfig.stationPointConfig.ssid, wifiConfig.stationPointConfig.pwd)
@@ -56,7 +55,7 @@ tmr.alarm(1, 5000, 1, function()
       collectgarbage()
    end
 end)
---[[ timer used
+--[[ timers used
 6 - for DS18B20 measurement
 5 - for DHT22 measurement
 4 - for BPM180 measurement
@@ -64,13 +63,13 @@ end)
 2 - free
 1 - free
 0 - free
---[[
+]]--
 -- start DS18B20 measuring and putting results into its global table
 if conf.sens_ds then
 	local dstemp=require("myds3")
-	ds_tab={}
+	ds_table={}
 	local ds_delay=conf.sens.ds_wait*1000
-	if ds_delay < 2000 or ds_delay > 3600000 then
+	if ds_delay < 60000 or ds_delay > 3600000 then
 		print("DS18B20 measuring timeout out of bounds, defaulting to 60s")
 		ds_delay=60000
 	else
@@ -81,20 +80,16 @@ if conf.sens_ds then
 	tmr.alarm(6, ds_delay,1,function()
 		dstemp.readT(conf.misc.ds_pin,function(r)
     		for k,v in pairs(r) do
-        		ds_tab[k]=v
+        		ds_table[k]=v
 			end
 		end)
 	end)
 end
 -- start DHT22 measuring and putting results into its global table
 if conf.sens_dht then
-
-end
--- start DHT22 measuring and putting results into its global table
-if conf.sens_dht then
-	dht_tab={}
+	dht_table={}
 	local dht_delay=conf.sens.dht_wait*1000
-	if dht_delay < 2000 or dht_delay > 3600000 then
+	if dht_delay < 60000 or dht_delay > 3600000 then
 		print("DHT22 measuring timeout out of bounds, defaulting to 60s")
 		dht_delay=60000
 	else
@@ -105,7 +100,8 @@ if conf.sens_dht then
 	tmr.alarm(5,dht_delay,1,function()
 		local status,temp,humi,temp_decimal,humi_decimal = dht.read(conf.sens.dht_pin)
 		if( status == dht.OK ) then
-			print("DHT Temperature:"..temp.."; ".."Humidity:"..humi,"")
+			--print("DHT Temperature:"..temp.."; ".."Humidity:"..humi,"")
+			dht_table = {temp,humi}
 		elseif( status == dht.ERROR_CHECKSUM ) then
 			print( "DHT Checksum error." );
 		elseif( status == dht.ERROR_TIMEOUT ) then
@@ -116,9 +112,9 @@ if conf.sens_dht then
 end
 -- start BMP180 measuring and putting results into its global table
 if conf.sens_bmp then
-	bmp_tab={}
+	bmp_table={}
 	local bmp_delay=conf.sens.bmp_wait*1000
-	if bmp_delay < 2000 or bmp_delay > 3600000 then
+	if bmp_delay < 60000 or bmp_delay > 3600000 then
 		print("BMP180 measuring timeout out of bounds, defaulting to 60s")
 		bmp_delay=60000
 	else
@@ -131,13 +127,12 @@ if conf.sens_bmp then
 		local t=string.format("%.2f",bmp085.temperature()/10)
 		local p=string.format("%.2f",bmp085.pressure(3)/100)
 		local al=string.format("%.2f",(p-101325)*843/10000)
-		bmp_tab = {t,p,al}
+		bmp_table = {t,p,al}
 end
-
--- start sending mqtt data
+-- connect to mqtt broker
 local mq=require("message2")
 local mdelay=conf.mqtt.delay*1000
-if mdelay < 5000 or mdelay > 3600000 then
+if mdelay < 60000 or mdelay > 3600000 then
 	print("MQTT delay out of bounds, defaulting to 60s")
 	mdelay=60000
 else
@@ -147,15 +142,21 @@ local client=mq.setup()
 -- start sending mqtt data for all sensors
 tmr.wdclr()
 tmr.alarm(3,mdelay,1,function()
-	for a,b in pairs(ds_tab) do
+	-- send ds18b20 data
+	for a,b in pairs(ds_table) do
 		local val=string.format("%.2f",b)
-		f.msgSend(client, conf.mqtt.topic.."/sensors/ds", cjson.encode({sensor=a, temp=val}))
+		f.msgSend(client, conf.mqtt.topic.."/sensors/ds18b20", cjson.encode({sensor=a, temp=val}))
 	end
-	for i=1,#bmp_tab do
-		f.msgSend(client, conf.mqtt.topic.."/sensors/bmp180",cjson.encode({temp=bmp_tab[i], pressure=bmp_tab[i+1], alt=bmp_tab[i+2]}))
+	-- send dht22 data
+	for i=1,#dht_table do
+		f.msgSend(client, conf.mqtt.topic.."/sensors/dht22",cjson.encode({temp=dht_table[i], humidity=dht_table[i+1]}))
+		i=i+2
+	end
+	-- send bmp180 data
+	for i=1,#bmp_table do
+		f.msgSend(client, conf.mqtt.topic.."/sensors/bmp180",cjson.encode({temp=bmp_table[i], pressure=bmp_table[i+1], alt=bmp_table[i+2]}))
 		i=i+3
 	end
 end)
 
 collectgarbage("collect")
-]]--
