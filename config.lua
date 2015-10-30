51a05c551f7f65040a8241eb889b566e21a914a2
Andrea Prunic <andrea@yu.net>
2015-10-30 16:50:06 +0100

    - change minimum timeouts to 60s

--- config.lua
+++ config.lua
@@ -21,16 +21,16 @@ local sens = {}
 -- DS18B20 temperature measurement sensor
 sens.ds_enable = false
 sens.ds_pin = 4 -- pin on which the sensors are connected
-sens.ds_wait = 5 -- measuring rate in seconds, decimal value allowed
+sens.ds_wait = 60 -- measuring rate in seconds, decimal value allowed
 -- DHT22 temperature and humidity sensor
 sens.dht_enable = true
 sens.dht_pin = 4 -- pin on which the sensor is connected
-sens.dht_wait = 5 -- measuring rate in seconds, decimal value allowed
+sens.dht_wait = 60 -- measuring rate in seconds, decimal value allowed
 -- BMP180 temperature and pressure sensor
 sens.bmp_enable = true
 sens.bmp_sda = 5 -- I2C sda pin
 sens.bmp_scl = 6 -- I2C scl pin
-sens.dht_wait = 5 -- measuring rate in seconds, decimal value allowed
+sens.dht_wait = 60 -- measuring rate in seconds, decimal value allowed
 conf.sens = sens
 -- mqtt settings
 local mqtt = {}
