939d950014787322ceaaf3557324250ef657773e
Andrea Prunic <andrea@yu.net>
2015-11-16 17:23:50 +0100

    - cleanup

--- myemoncms.lua
+++ myemoncms.lua
@@ -1,17 +1,18 @@
 -- emoncms library - writes data to emoncms
 local M
 
-conf = require("config")
+local conf = require("config")
 
 local function send(data)
 	local sk=net.createConnection(net.TCP, 0)
     local uri="GET "..conf.emon.path.."?node="..conf.emon.node.."&json="..data..
-		"&apikey="..conf.emon.apikey..
+        "&apikey="..conf.emon.apikey..
         " HTTP/1.1\r\nHost: "..conf.emon.server..
         "\r\nUser-Agent: esp8266\r\nAccept: */*\r\n\r\n"
 
     if conf.misc.debug then print(uri) end
-print(uri)
+--print(uri)
+print(node.heap())
 	sk:on("receive", function(sk, c)
 		print("Got "..c)
 	end)
@@ -32,4 +33,3 @@ end
 -- export functions
 M = { send = send }
 return M
--- curl -s "http://andrea.eunet.rs/emoncms/input/post.json?node=1&json=\{temp:25.0\}&apikey=c4088d0e6554f34c3e0310cc2c227038"
