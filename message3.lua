067025002f685d626535439af767183f0c8664e9
Andrea Prunic <andrea@yu.net>
2015-12-04 17:01:02 +0100

    - cleanup

--- message3.lua
+++ message3.lua
@@ -1,14 +1,4 @@
-local modname = ...
 local M = {}
-_G[modname] = M
-
-local tmr = tmr
-local mqtt = mqtt
-local wifi = wifi
---local print = print
--- Limited to local environment
-setfenv(1,M)
-
 local conf = require("config")
 local secure = conf.mqtt.secure
 if conf.mqtt.secure then
@@ -19,7 +9,11 @@ end
 
 local function msgSend(m, topic, msg)
 	local t=""
-    if topic == "" or topic == nil then t = conf.mqtt.topic end
+    if topic == "" or topic == nil then
+        t = conf.mqtt.topic
+    else
+        t = topic
+    end
     m:publish(t, msg, 0, 0, function(m)
         if conf.misc.debug then
             print("mqtt data sent")
@@ -29,12 +23,24 @@ local function msgSend(m, topic, msg)
     end)
 end
 -- received message, do something with it (control gpio or whatever)
-local function msgRecv(rtopic, data)
+local function msgRecv(m,rtopic, data)
+	local stat=nil
+	if conf.misc.status then stat = "button_on" else stat = "button_off" end
     print(rtopic..":"..data)
+    if string.find(data, "reqStatus") then
+--print("sending status: "..stat)
+		msgSend(m,conf.mqtt.topic, cjson.encode(stat))
+    end
+    if string.find(data, "toggleState") then
+        conf.misc.status = not conf.misc.status
+        if conf.misc.status then stat = "button_on" else stat = "button_off" end
+--print("sending status: "..stat)
+        msgSend(m,conf.mqtt.topic, cjson.encode(stat))
+    end
 end
 
 local function setup()
-    if conf.misc.debug then print("setup start") end
+    if conf.misc.debug then print("mqtt setup start") end
     m = mqtt.Client(conf.mqtt.clientid, 120, conf.mqtt.user, conf.mqtt.password)
     m:lwt("/lwt", conf.mqtt.clientid.." died", 0, 0)
 -- if we go offline
@@ -51,23 +57,17 @@ local function setup()
 	local rtopic=conf.mqtt.rtopic
     m:on("message", function(m, rtopic, data)
         if data ~= nil then
-            msgRecv(rtopic, data)
+            msgRecv(m,rtopic, data)
         end
     end)
 -- connect to broker and subscribe
-    tmr.wdclr()
-    tmr.alarm(1, 1000, 1, function()
-        if wifi.sta.status() == 5 then
-            tmr.stop(1)
-            m:connect(conf.mqtt.broker, conf.mqtt.port, secure, function(m)
-                print("connected to "..conf.mqtt.broker..":"..conf.mqtt.port)
-                m:subscribe(conf.mqtt.rtopic, 0, function(m)
-					msgSend(m, conf.mqtt.rtopic, conf.mqtt.clientid.." waiting for command")
-                end)
-            end)
-        end
+    m:connect(conf.mqtt.broker, conf.mqtt.port, secure, function(m)
+        print("connected to "..conf.mqtt.broker..":"..conf.mqtt.port)
+        m:subscribe(conf.mqtt.rtopic, 0, function(m)
+			msgSend(m, conf.mqtt.rtopic, conf.mqtt.clientid.." waiting for command")
+			if conf.misc.debug then print("mqtt setup end") end
+        end)
     end)
-    if conf.misc.debug then print("setup end") end
     return(m)
 end
 -- Return module table
