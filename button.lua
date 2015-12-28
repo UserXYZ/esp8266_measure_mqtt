750ea5330da9f083ec8882c519e42935fbc3bbea
Andrea Prunic <andrea@yu.net>
2015-12-28 01:26:03 +0100

    - added reaction to button

--- button.lua
+++ button.lua
@@ -4,7 +4,7 @@ local function debounce()
 	tmr.alarm(0, 50, 0, function()
 		if gpio.read(conf.misc.sw_pin) == 0 then
 			print("button pressed")
-			if conf.mqtt.use then -- send to mqtt broker
+			if conf.mqtt.use then -- send to mqtt broker, turn led on
 				mq.msgSend(client, conf.mqtt.topic, cjson.encode("led_on"))
 			end
 		end
