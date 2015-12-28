bf9e9228c60a9a0d9f988b9b09450215aff94ecd
Andrea Prunic <andrea@yu.net>
2015-12-28 01:28:27 +0100

    - add button reaction - remove extra code

--- message3.lua
+++ message3.lua
@@ -42,7 +42,7 @@ end
 local function setup()
     if conf.misc.debug then print("mqtt setup start") end
     m = mqtt.Client(conf.mqtt.clientid, 120, conf.mqtt.user, conf.mqtt.password)
-    m:lwt("/lwt", conf.mqtt.clientid.." died", 0, 0)
+    m:lwt("/lwt/"..conf.mqtt.topic, conf.mqtt.clientid.." died", 0, 0)
 -- if we go offline
     m:on("offline", function(m)
         print ("reconnecting to "..conf.mqtt.broker..":"..conf.mqtt.port)
