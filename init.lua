50f73029b158b3b7bf673619371e3e9953f332b6
Andrea Prunic <andrea@yu.net>
2016-07-28 10:43:28 +0200

    - setup display if enabled in config.lua - change messages to strings that can be reused by print and display.disp_stat when displaying status messages

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 -- prepare files
 local compileAndRemoveIfNeeded = function(f)
-   if file.open(f) then
-      file.close()
-      print('Compiling:', f)
-      node.compile(f)
-      file.remove(f)
-      collectgarbage()
-   end
+    if file.open(f) then
+	file.close()
+	print('Compiling:', f)
+	node.compile(f)
+	file.remove(f)
+	collectgarbage()
+    end
 end
 -- main()
-local serverFiles = {'dns.lua', 'telnet.lua','main3.lua','message3.lua', 'myds3.lua', 'myNtpTime.lua', 'getDST.lua', 'myemoncms.lua', 'button.lua'}
+local serverFiles = {'dns.lua', 'telnet.lua','main3.lua','message3.lua', 'myds3.lua', 'myNtpTime.lua', 'getDST.lua', 'myemoncms.lua', 'button.lua', 'display.lua'}
 for i, f in ipairs(serverFiles) do
     compileAndRemoveIfNeeded(f)
 end
@@ -28,27 +28,45 @@ wifi.sta.config(conf.wlan.ssid, conf.wlan.pwd)
 wifiConfig = nil
 collectgarbage()
 
+if conf.misc.use_display then
+    display = require("display")
+    display.setup()
+    display.cls()
+end
+
 local joinCounter = 0
 local joinMaxAttempts = 20
 tmr.alarm(1, 5000, 1, function()
-   local ip = wifi.sta.getip()
-   if ip == nil and joinCounter < joinMaxAttempts then
-      print('Connecting to WiFi Access Point ...')
-      joinCounter = joinCounter +1
-   else
-      if joinCounter == joinMaxAttempts then
-         print('Failed to connect to WiFi Access Point.')
-      else
-         print('Got IP: ',ip)
-         print('heap: ',node.heap())
+    local ip = wifi.sta.getip()
+    if ip == nil and joinCounter < joinMaxAttempts then
+	local msg="Connecting to WiFi Access Point..."
+	print(msg)
+	if conf.misc.use_display then
+	    display.disp_stat(msg)
+	end
+	joinCounter = joinCounter +1
+    else
+	if joinCounter == joinMaxAttempts then
+	    local msg="Failed to connect to WiFi Access Point"
+	    print(msg)
+	    if conf.misc.use_display then
+		display.disp_stat(msg)
+	    end
+	else
+	    local msg="Got IP: "..ip
+	    print(msg)
+	    if conf.misc.use_display then
+		display.disp_stat(msg)
+	    end
+	    print('heap: ',node.heap())
          -- Uncomment to automatically start everything
             --dofile("telnet.lc")
-            dofile("main3.lc")
-            dofile("button.lc")
-      end
-      tmr.stop(1)
-      joinCounter = nil
-      joinMaxAttempts = nil
-      collectgarbage()
-   end
+	    dofile("main3.lc")
+	    dofile("button.lc")
+	end
+	tmr.stop(1)
+	joinCounter = nil
+	joinMaxAttempts = nil
+	collectgarbage()
+    end
 end)
